99446959ab2ddd58cf08ac4e04ed8a2273d85bdf
Mr. Robot <email@email.com>
2019-05-15 19:16:49 +0200

    sorry about that, git lied to me

--- main.py
+++ main.py
@@ -12,19 +12,6 @@ def one_hot(y, n_classes):
         result[i, y[i]] = 1
     return result
 
-<<<<<<< HEAD
-def oneHot(y, n_class):
-    result = np.zeros(y.shape[0], n_class)
-    for i in range(0, y.shape[0]):
-        result[i][y[i]] = 1
-    return result
-    
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-
-y_train = oneHot(y_train, 10)
-y_test = oneHot(y_test, 10)
-
-=======
 np.random.seed(1000)
 (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
@@ -32,34 +19,27 @@ y_train = one_hot(y_train, 10)
 y_test = one_hot(y_test, 10)
 
 '''
->>>>>>> 7621c9190145ebe21205adc4de2d6e9757e0f8d7
 #Instantiate an empty model
 model = Sequential()
-
 # 1st Convolutional Layer
 model.add(Conv2D(filters=32, input_shape=(32,32,3), kernel_size=(5,5), strides=(1,1), padding='valid'))
 model.add(Activation('relu'))
-
 # 2nd Convolutional Layer
 model.add(Conv2D(filters=64, kernel_size=(5,5), strides=(1,1), padding='valid'))
 model.add(Activation('relu'))
 # Max Pooling
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
-
 # 3rd Convolutional Layer
 model.add(Conv2D(filters=96, kernel_size=(3,3), strides=(1,1), padding='valid'))
 model.add(Activation('relu'))
-
 # 4th Convolutional Layer
 model.add(Conv2D(filters=96, kernel_size=(3,3), strides=(1,1), padding='valid'))
 model.add(Activation('relu'))
-
 # 5th Convolutional Layer
 model.add(Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding='valid'))
 model.add(Activation('relu'))
 # Max Pooling
 model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
-
 # Passing it to a Fully Connected layer
 model.add(Flatten())
 # 1st Fully Connected Layer
@@ -67,19 +47,16 @@ model.add(Dense(1024, input_shape=(4096,)))
 model.add(Activation('relu'))
 # Add Dropout to prevent overfitting
 model.add(Dropout(0.4))
-
 # 2nd Fully Connected Layer
 model.add(Dense(512))
 model.add(Activation('relu'))
 # Add Dropout
 model.add(Dropout(0.4))
-
 # 3rd Fully Connected Layer
 model.add(Dense(10))
 model.add(Activation('relu'))
 # Add Dropout
 model.add(Dropout(0.4))
-
 # Output Layer
 model.add(Dense(10))
 model.add(Activation('softmax'))
